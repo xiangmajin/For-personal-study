80fc17c051af622589cc01a9446e814ed39ba75c
22519 <2251914062@qq.com>
2025-04-09 11:11:26 +0800

    第一次测试

---
+++ src/main/java/org/example/skytakeout/SkyTakeoutApplication.java
@@ -0,0 +1,13 @@
+package org.example.skytakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyTakeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyTakeoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/skytakeout/SkyTakeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skytakeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTakeoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
